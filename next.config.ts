f8ef4b74df7eafad2edef3cacead09ccbe93f93a
tomaslanterna <tomaslanterna@gmail.com>
2026-08-24 02:09:06 -0300

    fix: add env vars config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    BACKEND_PUBLIC_API_URL: process.env.BACKEND_PUBLIC_API_URL,
+  },
+  // @ts-ignore
   allowedDevOrigins: ["192.168.1.47", "http://192.168.1.47:3000"],
 };
 
